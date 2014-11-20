cd0facd0bc5ea52e23d2a360806158d4ff9aceb7
Simon Ninon <simon.ninon@gmail.com>
2014-11-20 12:20:56 +0100

    Test of the observer class

--- R-Type/server/sources/main.cpp
+++ R-Type/server/sources/main.cpp
@@ -6,33 +6,40 @@ void fct(void) {
 	std::cout << "work done" << std::endl;
 }
 
+class Test {
+
+	public:
+		enum class Events {
+			SIMPLE_EVENT
+		};
+
+	public:
+		void registerObserver(Events e, std::function<void()> fct) {
+			observer.registerObserver(e, fct);
+		}
+
+		void notifyObserver(Events e) {
+			observer.notifyObservers(e);
+		}
+
+	private:
+		Observer<Events> observer;
+
+};
+
+void test(void) {
+	std::cout << "TEST OK" << std::endl;
+}
+
 int main(void) {
-	ThreadPool pool(5);
+	Test t;
 
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
-	pool << fct;
+	t.registerObserver(Test::Events::SIMPLE_EVENT, test);
+	t.registerObserver(Test::Events::SIMPLE_EVENT, test);
+	t.registerObserver(Test::Events::SIMPLE_EVENT, test);
+	t.notifyObserver(Test::Events::SIMPLE_EVENT);
 
-	while (true);
+	system("PAUSE");
+
+	return 0;
 }
