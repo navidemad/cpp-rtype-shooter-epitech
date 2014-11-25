c9ff0eda892db6f4645c4d27b3e749ad26569385
YiuRULE <guillaume.tassery@epitech.eu>
2014-11-25 10:48:45 +0100

    Add good RTypeClient.(hpp/cpp)

--- R-Type/client/includes/RTypeClient.hpp
+++ R-Type/client/includes/RTypeClient.hpp
@@ -1,7 +1,19 @@
 #pragma once
 
+
+#include "Engine/ECSManager.hpp"
+
+class ECSManager;
+
 class RTypeClient
 {
+public:
+	enum Game : unsigned int
+	{
+		MENU = 0,
+		RTYPE = 1
+	};
+	
 	// ctor - dtor
 	public:
 		RTypeClient();
@@ -9,6 +21,19 @@ class RTypeClient
 
 	// coplien form
 	private:
-		RTypeClient(RTypeClient const &) {}
-		RTypeClient const	&operator=(RTypeClient const &) { return *this; }
+		RTypeClient(RTypeClient const &);
+		RTypeClient const	&operator=(RTypeClient const &);
+
+	public:
+		void run();
+
+	// setter getter
+	public: 
+		void			setIdGame(unsigned int);
+		unsigned int	getIdGame() const;
+
+	// Attribute
+	private:
+		unsigned int	mCurrentId;
+		ECSManager		Engine;
 };
--- R-Type/client/sources/RTypeClient.cpp
+++ R-Type/client/sources/RTypeClient.cpp
@@ -1,11 +1,31 @@
 #include "RTypeClient.hpp"
+#include <iostream>
 
 RTypeClient::RTypeClient()
+: mCurrentId(RTypeClient::MENU), Engine(this)
 {
-
 }
 
 RTypeClient::~RTypeClient()
 {
 
+}
+
+RTypeClient::RTypeClient(RTypeClient const &) : Engine(this) {}
+
+RTypeClient const	&RTypeClient::operator=(RTypeClient const &) { return *this; }
+
+void	RTypeClient::run()
+{
+	
+}
+
+void	RTypeClient::setIdGame(unsigned int currentId)
+{
+	mCurrentId = currentId;
+}
+
+unsigned int	RTypeClient::getIdGame() const
+{
+	return mCurrentId;
 }
