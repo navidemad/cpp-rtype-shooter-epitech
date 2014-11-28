27ac93a21a8771ddeb748b0c2ec46ea6bbafa9a6
Rainiugnas <pguego@gmail.com>
2014-11-28 05:50:37 -0800

    Add CommandDelleteGame + change CommandCreateGame

---
+++ R-Type/client/sources/Command/CommandDeleteGame.cpp
@@ -0,0 +1,22 @@
+#include "CommandDeleteGame.hpp"
+
+IClientSocket::Message CommandDeleteGame::getMessage(void) const {
+	CommandDeleteGame::PacketFromClient packet;
+	std::memset(&packet, 0, sizeof(CommandDeleteGame::PacketFromClient));
+
+	std::copy(mName.begin(), mName.begin() + std::min(mName.size(), sizeof(packet.name) - 1), packet.name);
+
+	IClientSocket::Message message;
+	message.msg.assign(reinterpret_cast<char *>(&packet), reinterpret_cast<char *>(&packet + 1));
+	message.msgSize = sizeof(CommandDeleteGame::PacketFromClient);
+
+	return message;
+}
+
+unsigned int CommandDeleteGame::getSizeToRead(void) const {
+	throw std::string("This command can not be receive by the client");
+}
+
+void CommandDeleteGame::initFromMessage(const IClientSocket::Message &message) {
+	throw std::string("This command can not be receive by the client");
+}
