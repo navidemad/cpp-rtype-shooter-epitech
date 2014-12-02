3974dacd1ff0eaceb4534d6418b1b2bc1107fec4
Alain NGUYEN <alain.nguyen@epitech.eu>
2014-12-02 16:36:49 +0100

    Move All file Script to Share Folder for GUIGUI LA LICEMA

---
+++ R-Type/shared/includes/Script/ScriptAction.hpp
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <string>
+#include "IScriptCommand.hpp"
+
+class ScriptAction : public IScriptCommand {
+	// virtual destructor
+	public:
+		explicit ScriptAction(void) { }
+		~ScriptAction(void) { }
+
+	// public pure methods
+	public:
+		IScriptCommand::Instruction	getInstruction(void) const { return IScriptCommand::Instruction::ACTION; }
+
+	// actions enum
+	public:
+		enum class TYPE { SPAWN_MOB, MOVE_MOB };
+
+	// interface action class
+	public:
+		class IActionType {
+			public:
+				virtual ~IActionType(void) {}
+		};
+
+	// getter-setter
+	public:
+		int								getActionFrame(void) const { return mActionFrame; }
+		const std::string&				getActionMobAction(void) const { return mActionMobAction; }
+		ScriptAction::TYPE				getActionType(void) const { return mType; }
+		std::shared_ptr<IActionType>	getActionParams(void) const { return mParams; }
+
+		void							setActionFrame(const int& frame) { mActionFrame = frame; }
+		void							setActionMobAction(const std::string& mobAction) { mActionMobAction = mobAction; }
+		void							setActionType(ScriptAction::TYPE type) { mType = type; }
+		void							setActionParams(std::shared_ptr<IActionType> params) { mParams = params; }
+
+	// actions class
+	public:
+		class SpawnMob : public IActionType {
+
+			// ctor / dtor
+			public:
+				SpawnMob(void) { }
+				~SpawnMob(void) { }
+
+			// getter-setter
+			public:
+				int						getActionIdMonster(void) const { return mIdMonster; }
+				std::string				getActionName(void) const { return mName; }
+				int						getActionXpos(void) const { return mXpos; }
+				int						getActionYpos(void) const { return mYpos; }
+				int						getActionAngle(void) const { return mAngle; }
+
+				void					setActionIdMonster(const int& idMonster) { mIdMonster = idMonster; }
+				void					setActionName(const std::string& name) { mName = name; }
+				void					setActionXpos(const int& xPos) { mXpos = xPos; }
+				void					setActionYpos(const int& yPos) { mYpos = yPos; }
+				void					setActionAngle(const int& angle) { mAngle = angle; }
+
+			// attributes
+			public:
+				int mIdMonster;
+				std::string mName;
+				int mXpos;
+				int mYpos;
+				int mAngle;
+		};
+
+		class MoveMob : public IActionType {
+
+			// ctor / dtor
+			public:
+				MoveMob(void) { }
+				~MoveMob(void) { }
+
+			// getter-setter
+			public:
+				int						getActionIdMonster(void) const { return mIdMonster; }
+				int						getActionAngle(void) const { return mAngle; }
+
+				void					setActionIdMonster(const int& idMonster) { mIdMonster = idMonster; }
+				void					setActionAngle(const int& angle) { mAngle = angle; }
+
+			// attributes
+			public:
+				int						mIdMonster;
+				int						mAngle;
+		};
+
+	// attribut
+	public:
+		int								mActionFrame;
+		std::string						mActionMobAction;
+		std::shared_ptr<IActionType>	mParams;
+		ScriptAction::TYPE				mType;
+};
