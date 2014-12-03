a85f138677e3c39504bae76ee5a1e915634d6a6b
Alain NGUYEN <alain.nguyen@epitech.eu>
2014-12-03 02:14:41 +0100

    Abstraction Readdir pour UNIX

--- R-Type/server/sources/Readdir/UnixReaddir.cpp
+++ R-Type/server/sources/Readdir/UnixReaddir.cpp
@@ -1,3 +1,7 @@
+#include <dirent.h>
+#include <iostream>
+#include <string>
+#include <list>
 #include "UnixReaddir.hpp"
 
 UnixReaddir::UnixReaddir(void) {
@@ -7,7 +11,16 @@ UnixReaddir::~UnixReaddir(void) {
 }
 
 std::list<std::string> UnixReaddir::readFolder(std::string pathFolder){
-	std::list<std::string> files;
-	
-	return files;
+  DIR *dir;
+  struct dirent *ent;
+  std::list<std::string> files;
+ 
+  if ((dir = opendir (pathFolder)) != NULL)
+    {
+      while ((ent = readdir(dir)) != NULL)
+        if (strcmp(ent->d_name, ".") && strcmp(ent->d_name, ".."))
+          files.push_back(ent->d_name);
+      closedir (dir);
+    }
+  return files;
 }
