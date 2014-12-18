191ea1a785ca894a3331019717b609eac0e01a3c
yiurule <guillaume.tassery@epitech.eu>
2014-12-18 15:49:59 +0100

    add dlloader in main for navid

--- R-Type/client/sources/main.cpp
+++ R-Type/client/sources/main.cpp
@@ -1,12 +1,22 @@
 #include <QtCore>
 #include "IResource.hpp"
 #include "RTypeClient.hpp"
+#include "DynLib/WindowsDynLib.hpp"
 
 Q_DECLARE_METATYPE(std::string)
 Q_DECLARE_METATYPE(IResource::Direction)
 
 int main(int ac, char **av)
 {
+	/*
+	auto            lib = new WindowsDynLib;
+	void*           fctPtr;
+
+	lib->libraryLoad(std::string("entity/LOL"));
+	fctPtr = lib->functionLoad("test");
+	std::cout << fctPtr << std::endl;
+	((void(*)(void))fctPtr)();
+	*/
     QCoreApplication app(ac, av);
 
     qRegisterMetaType<std::string>();
@@ -15,5 +25,5 @@ int main(int ac, char **av)
     QObject::connect(&rtype, SIGNAL(finished()), &app, SLOT(quit()));
 
     rtype.start();
-    return app.exec();
+	return app.exec();
 }
